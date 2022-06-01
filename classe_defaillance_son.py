60b3a7f575d1a6e557adcd2008df17f3f562ff7b
gabriel <aillet.gabriel05@gmail.com>
2022-06-01 14:54:04 +0200

    test

--- classe_defaillance_son.py
+++ classe_defaillance_son.py
@@ -7,6 +7,7 @@ import threading
 from tkinter import ttk
 
 
+
 class classe_defaillance_son:
     def __init__(self,son):
         self.son = son
@@ -43,9 +44,5 @@ class classe_defaillance_son:
 
 
 if __name__ == '__main__':
-    a = classe_defaillance_son('example.wav')
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    root.mainloop()
+    print('q')
+
